92a3ac9a3df10d790b9f30908bc68b201203780e
joshnos <joshuanostas49@gmail.com>
2020-05-26 09:50:56 -0400

    add listOfUsers code

--- src/app/users/list-of-users/list-of-users.component.ts
+++ src/app/users/list-of-users/list-of-users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/User';
 
 @Component({
   selector: 'app-list-of-users',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-of-users.component.css']
 })
 export class ListOfUsersComponent implements OnInit {
+
+  users: User[] = [{
+    imgUrl: "https://avatars0.githubusercontent.com/u/1?v=4",
+    name: "mojombo",
+    gitHubUrl: "https://github.com/mojombo"
+  }];
 
   constructor() { }
 
