5ec096fa9dd85cf31e37a82c02b296e49f848b9a
joshnos <joshuanostas49@gmail.com>
2020-05-26 13:14:45 -0400

    add http service code

--- src/app/shared/services/http.service.ts
+++ src/app/shared/services/http.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpService {
 
-  constructor() { }
+  private httpOptions: any;
+  private commonUrl: any = 'https://api.github.com/';
+
+  constructor(private httpClient: HttpClient) { 
+    this.setHttpOptions();
+  }
+
+  private setHttpOptions() {
+    this.httpOptions = {};
+  }
+
+  public getElement(url: string){
+    return this.httpClient.get(this.commonUrl + url, this.httpOptions);
+  }
+
+  public getElementById(url: string, id: string) {
+      return this.httpClient.get(this.commonUrl + url + '/' + id, this.httpOptions).subscribe(
+        error =>{
+            handleError(error);
+        });
+  }
+
+}
+
+function handleError(error) {
+  console.error('An error occurred', error);
 }
