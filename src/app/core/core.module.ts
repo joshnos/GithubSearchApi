343ff01788d18205d180bd3d7a9e93b3dbc54190
joshnos <joshuanostas49@gmail.com>
2020-05-26 01:10:49 -0400

    add navbar component

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { WrapperComponent } from './wrapper/wrapper.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 
 @NgModule({
   declarations: [
-    WrapperComponent
+    WrapperComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule,
