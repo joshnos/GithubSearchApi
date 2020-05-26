b66c1f0a47334efd5749a68c94c48aaf6d0aee3a
joshnos <joshuanostas49@gmail.com>
2020-05-26 09:22:32 -0400

    add listoOfUsers and user components

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UsersRoutingModule } from './users-routing.module';
+import { ListOfUsersComponent } from './list-of-users/list-of-users.component';
+import { UserComponent } from './user/user.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ListOfUsersComponent, UserComponent],
   imports: [
     CommonModule,
     UsersRoutingModule
