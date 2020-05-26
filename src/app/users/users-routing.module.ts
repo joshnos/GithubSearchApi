43216ec85e6c9792b97805d1fe0163606d3076ff
joshnos <joshuanostas49@gmail.com>
2020-05-26 09:56:58 -0400

    add users route

--- src/app/users/users-routing.module.ts
+++ src/app/users/users-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListOfUsersComponent } from './list-of-users/list-of-users.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListOfUsersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
