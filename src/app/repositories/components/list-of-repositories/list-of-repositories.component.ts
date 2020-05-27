d2105d353e2d7692ec16c0d505b30c2f40e230ca
joshnos <joshuanostas49@gmail.com>
2020-05-26 23:39:51 -0400

    add components: listOfRepositories and repository to repositories module

---
+++ src/app/repositories/components/list-of-repositories/list-of-repositories.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-of-repositories',
+  templateUrl: './list-of-repositories.component.html',
+  styleUrls: ['./list-of-repositories.component.css']
+})
+export class ListOfRepositoriesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/repositories/repositories.module.ts
+++ src/app/repositories/repositories.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { RepositoriesRoutingModule } from './repositories-routing.module';
+import { ListOfRepositoriesComponent } from './components/list-of-repositories/list-of-repositories.component';
+import { RepositoryComponent } from './components/repository/repository.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ListOfRepositoriesComponent, RepositoryComponent],
   imports: [
     CommonModule,
     RepositoriesRoutingModule
