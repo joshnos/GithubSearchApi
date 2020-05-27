4c4c78a947c0680bd96f1cb073263b5cb6f37418
joshnos <joshuanostas49@gmail.com>
2020-05-26 23:56:41 -0400

    add repository component code

--- src/app/repositories/components/repository/repository.component.ts
+++ src/app/repositories/components/repository/repository.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Repository } from '../../models/Repository';
 
 @Component({
   selector: 'app-repository',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repository.component.css']
 })
 export class RepositoryComponent implements OnInit {
+
+  @Input() repository: Repository;
 
   constructor() { }
 
