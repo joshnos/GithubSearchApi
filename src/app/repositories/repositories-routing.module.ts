8454e0c823ca884805754287ced07650773a48cd
joshnos <joshuanostas49@gmail.com>
2020-05-26 23:51:13 -0400

    add routes to repositories module

--- src/app/repositories/repositories-routing.module.ts
+++ src/app/repositories/repositories-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListOfRepositoriesComponent } from './components/list-of-repositories/list-of-repositories.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ListOfRepositoriesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
